71fb6940eb37b5c771f400ca69c73607a0f39499
Nicola Clark <nicola@slottedspoon.dev>
2024-09-27 22:57:20 -0500

    move shared stuff to +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
   import '../app.css';
 </script>
 
-<slot />
+<main
+  class="bg-slate-200 flex flex-col gap-4 h-full items-center justify-center mx-auto px-3 py-4 w-5/6 lg:w-2/3"
+>
+  <slot />
+</main>
--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -5,10 +5,6 @@
 <svelte:head>
   <title>ao3pub &dash; {data.workID}</title>
 </svelte:head>
-<main
-  class="bg-slate-200 flex flex-col gap-4 h-full items-center justify-center mx-auto px-3 py-4 w-5/6 lg:w-2/3"
->
-  <h1 class="font-bold text-3xl">ao3pub editor</h1>
-  <h2 class="font-semibold text-2xl">{data.workID}</h2>
-  <p>{data.metadata}</p>
-</main>
+<h1 class="font-bold text-3xl">ao3pub editor</h1>
+<h2 class="font-semibold text-2xl">{data.workID}</h2>
+<p>{data.metadata}</p>
