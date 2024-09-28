9a9ce283e18454c3287e72a31041509ac4830ca1
Nicola Clark <nicola@slottedspoon.dev>
2024-09-27 23:00:34 -0500

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  <title>ao3pub &dash; error!</title>
+</svelte:head>
+<h1 class="font-bold text-3xl">ao3pub</h1>
+<h2 class="font-semibold text-2xl">{$page.status} error</h2>
+{#if $page.error}
+  <p class="error-text">{$page.error.message}</p>
+{/if}
