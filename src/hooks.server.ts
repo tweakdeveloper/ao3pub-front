7d0ef0a075e67012978abf388a2302370d65ba68
Nicola Clark <nicola@slottedspoon.dev>
2024-10-03 16:00:11 -0500

    oops
    forgot to rewrite HTTP path from `/api` to `/` in the hook.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,9 @@
 export async function handleFetch({ fetch, request }) {
   if (request.url.startsWith('http://localhost:8080/api')) {
-    request = new Request(request.url.replace('http://localhost', 'http://back'), request);
+    request = new Request(
+      request.url.replace('http://localhost:8080/api', 'http://back:8080'),
+      request,
+    );
   }
   return fetch(request);
 }
