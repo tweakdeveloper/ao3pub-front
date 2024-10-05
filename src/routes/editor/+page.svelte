1b6bb369d518e1f70fb507be6badc99154cd9333
Nicola Clark <nicola@slottedspoon.dev>
2024-10-05 12:41:37 -0500

    change editor page title

--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <svelte:head>
-  <title>ao3pub &dash; {data.workID}</title>
+  <title>ao3pub &dash; editor: {data.metadata.title}</title>
 </svelte:head>
-<h1 class="font-bold text-3xl">ao3pub editor</h1>
+<h1 class="font-bold text-3xl">ao3pub &dash; editor</h1>
 <h2 class="font-semibold text-2xl">{data.workID}</h2>
 <p>{data.metadata.title}</p>
